fff88a423c267509a1ec677f12e5d91367cebda7
impulse <ephraim@gmail.com>
2025-04-01 17:58:23 +0100

    modification du champ photo dans la table article

--- database/migrations/2025_03_05_114133_create_articles_table.php
+++ database/migrations/2025_03_05_114133_create_articles_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('title');
             $table->string('slug');
-            $table->string('photo');
+            $table->longText('photo');
             $table->string('auteur', 40);
             $table->longText('content');
             $table->timestamps();
